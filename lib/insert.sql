52f4cd4c108f5277c13bef0c320d5558fe751e8b
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 01:00:36 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
+
+
+
+INSERT INTO projects VALUES ("January", "Movie", "5/1/19", "5/1/24", 100000);
+INSERT INTO projects VALUES ("February", "Music", "6/1/19", "6/1/24", 250000);
+INSERT INTO projects VALUES ("March", "Movie", "9/1/20", "9/1/21", 550000);
+INSERT INTO projects VALUES ("April", "Movie", "3/19/19", "3/21/21", 854300);
+INSERT INTO projects VALUES ("May", "Music", "9/9/19", "10/10/20", 2000);
+INSERT INTO projects VALUES ("June", "web series", "6/1/19", "6/25/20", 600000);
+INSERT INTO projects VALUES ("July", "Movie", "3/1/19", "3/1/29", 100000);
+INSERT INTO projects VALUES ("August", "web series", "5/11/19", "5/1/20", 100000);
+INSERT INTO projects VALUES ("September", "web series", "9/1/19", "9/1/24", 800000);
+INSERT INTO projects VALUES ("October", "Movie", "10/10/19", "10/10/20", 670000);
+
+INSERT INTO users VALUES ("Melissa", 29)
+INSERT INTO users VALUES ("Alli", 21)
+INSERT INTO users VALUES ("Chad", 27)
+INSERT INTO users VALUES ("Todd", 25)
+INSERT INTO users VALUES ("Reagan", 20)
+INSERT INTO users VALUES ("Noah", 21)
+INSERT INTO users VALUES ("Ella", 23)
+INSERT INTO users VALUES ("Mark", 30)
+INSERT INTO users VALUES ("Gina", 30)
+INSERT INTO users VALUES ("Ambre", 25)
+INSERT INTO users VALUES ("Aubrey", 26)
+INSERT INTO users VALUES ("Billy", 24)
+INSERT INTO users VALUES ("Matt", 31)
+INSERT INTO users VALUES ("Lydia", 20)
+INSERT INTO users VALUES ("Max", 21)
+INSERT INTO users VALUES ("Luke", 40)
+INSERT INTO users VALUES ("Starr", 41)
+INSERT INTO users VALUES ("Rachel", 19)
+INSERT INTO users VALUES ("Mitch", 28)
+INSERT INTO users VALUES ("Zeke", 23)
