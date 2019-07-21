efa1f1ff7055ab44958c4d09cfe8d58f4f13784d
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 01:43:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, SUM(pledges.amount) FROM projects
- LEFT JOIN peldges ON pledges.project_id = projects.id
+ LEFT JOIN pledges ON pledges.project_id = projects.id
  GROUP BY pledges.project_id
  ORDER BY projects.title";
 end
