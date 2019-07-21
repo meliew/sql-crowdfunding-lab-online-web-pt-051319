a724027fadb80b8bd17e2678b256a042042588f3
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 01:19:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
 
 
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("January", "Movie", 100000, "5/1/19", "5/1/24");
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("February", "Music", 250000, "6/1/19", "6/1/24");
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("March", "Movie", 550000, "9/1/20", "9/1/21");
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("April", "Movie", 854300, "3/19/19", "3/21/21");
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("May", "Music", 2000, "9/9/19", "10/10/20");
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("June", "web series", 600000, "6/1/19", "6/25/20");
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("July", "Movie", 100000, "3/1/19", "3/1/29");
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("August", "web series", 100000, "5/11/19", "5/1/20");
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("September", "web series", 800000, "9/1/19", "9/1/24");
-INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) ("October", "Movie", 670000, "10/10/19", "10/10/20");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (1, "January", "Movie", 100000, "5/1/19", "5/1/24");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (2, "February", "Music", 250000, "6/1/19", "6/1/24");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (3, "March", "Movie", 550000, "9/1/20", "9/1/21");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (4, "April", "Movie", 854300, "3/19/19", "3/21/21");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (5, "May", "Music", 2000, "9/9/19", "10/10/20");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (6, "June", "web series", 600000, "6/1/19", "6/25/20");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (7, "July", "Movie", 100000, "3/1/19", "3/1/29");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (8, "August", "web series", 100000, "5/11/19", "5/1/20");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (9, "September", "web series", 800000, "9/1/19", "9/1/24");
+INSERT INTO projects VALUES (id, title, category, funding_goal, start_date, end_date) (10, "October", "Movie", 670000, "10/10/19", "10/10/20");
 
 INSERT INTO users VALUES ("Melissa", 29)
 INSERT INTO users VALUES ("Alli", 21)
