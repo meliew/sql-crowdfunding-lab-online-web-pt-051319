f67b39da4d8e34b1cc1b0a8457b8eb04286278c2
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 01:55:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,11 @@ ORDER BY SUM(pledges.amount)";
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+  #category names
+  #pledge amounts for all pledges in music category
+"SELECT projects.category
+FROM projects
+WHERE projects.category = "music""
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
