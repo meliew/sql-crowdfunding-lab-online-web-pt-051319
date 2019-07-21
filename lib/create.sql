6033913167e1854e63cc193c22e8136eaf193811
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 01:14:52 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, start_date TEXT, end_date TEXT, funding_goal INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT,  funding_goal INTEGER, start_date TEXT, end_date TEXT);
 
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 
