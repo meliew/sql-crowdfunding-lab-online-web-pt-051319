e288c356452096429a00835cd0610b5b83665b52
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 00:51:38 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, start_date TEXT, end_date TEXT);
 
-CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 
-CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project INTEGER)
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project INTEGER);
