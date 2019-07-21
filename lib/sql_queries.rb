c0fed387f74015d40e9f273f629c5549f3177d15
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 01:57:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,9 +39,10 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   #category names
   #pledge amounts for all pledges in music category
 "SELECT projects.category
-SUM(pledges.amounts)
+pledges.amounts
 FROM projects
-WHERE projects.category = music";
+INNER JOIN pledges ON pledges.project_id = projects.id
+WHERE projects.category = 'music'";
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
