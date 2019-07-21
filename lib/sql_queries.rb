5f464c2bc0b9268e4f9fef04621140f4b334c0fc
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 01:49:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,10 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, amounts.pledges, "
+"SELECT users.name, SUM(amounts.pledges) FROM users
+INNER JOIN pledges ON pledges.user_id = users.id
+GROUP BY users.name
+ORDER BY SUM(pledges.amount) "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
